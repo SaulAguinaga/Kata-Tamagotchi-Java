f4eb828664e8ff7fc2b9111323c94e29eba05418
SaulAguinaga <saul_aguinaga@hotmail.es>
2023-02-01 14:36:35 +0100

    cheking energy and mood

--- src/main/java/com/kata/App.java
+++ src/main/java/com/kata/App.java
@@ -1,17 +1,41 @@
 package com.kata;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
         Tamagotchi tamagochi = new Tamagotchi();
+        Integer option;
+        do {
+            option=0;
+            System.out.println(" 1. Eat \n 2. Play \n 3. Sleep \n 4. Poop \n 5. Status \n 6. Exit \n ");
+            Scanner qwerty = new Scanner(System.in);
+            option = Integer.parseInt(qwerty.next());
 
-        tamagochi.eating();
-        tamagochi.playing();
-        
+        if (option == 1) {
+            tamagochi.eating();
+            System.out.println(option);      
+        }
+        if (option == 2) {
+            tamagochi.playing();
+            System.out.println(option); 
+        }
+        if (option == 3) {
+            tamagochi.sleeping();
+        }            
+        if (option == 4) {
+            tamagochi.poop();
+        }
+        if (option == 5) {
+            tamagochi.status(); 
+        }
+        if (option == 6) {
+            qwerty.close();
+        }
+
+        } while (option != 6);
     }
 }
--- src/main/java/com/kata/Tamagotchi.java
+++ src/main/java/com/kata/Tamagotchi.java
@@ -12,24 +12,55 @@ public class Tamagotchi {
         
     }
 
+    private void checking() {
+        if (energy < 3 && energy > 0) {
+            System.out.println("Energy: (-_-)");
+        }
+        if(energy == 0){
+            System.out.println("Energy: (-_-)zZZ");
+        }
+        if(energy >= 3){
+            System.out.println("Energy: :-|");
+        }
+        if(mood <= 8 && mood >=2){
+            System.out.println("Mood: :-|");
+        }
+        if (mood > 8) {
+            System.out.println("Mood: :-)");
+        }
+        if (mood < 2) {
+            System.out.println("Mood: ఠ_ఠ");
+        }
+    }
+
     public void eating(){
         hunger -= 2;
         energy ++;
         fullness ++;
+        checking();
     }
     public void playing(){
         mood ++;
         hunger ++;
         energy --;
+        checking();
     }
     public void sleeping(){
         energy += 2;
+        checking();
     }
     public void poop(){
         fullness = 0;
+        checking();
     }
+    public void status(){
+        System.out.println("Tamagotchi \n mood= " + mood + "\n hunger= " + hunger + "\n energy= " + energy + "\n fullness= " + fullness + "\n");
+    }  
 
-
+    @Override
+    public String toString() {
+        return "Tamagotchi \n mood=" + mood + "\n hunger=" + hunger + "\n energy=" + energy + "\n fullness=" + fullness + "\n";
+    }
 
     public int getMood() {
         return mood;
@@ -63,5 +94,6 @@ public class Tamagotchi {
     public void setFullness(int fullness) {
         this.fullness = fullness;
     }
+
     
 }
